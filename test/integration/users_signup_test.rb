8a8da60d42bb5fc7f192f777c7defef61aa63ed5
Uri Feld <ufeld@us.ibm.com>
2016-04-08 20:16:34 -0400

    Done for the day

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,8 +10,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password:              "foo",
                                password_confirmation: "bar" }
     end
-    assert_template 'users/new'
-  end
+    assert_template 'users/new' 
+ 
+ end
 
   test "valid signup information" do
     get signup_path
